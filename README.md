5c7b8317f8ed74356aeed0aaa4be501ce18d9c27
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-02-26 18:19:47 +1100

    Layout

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # special-computing-machine
+
+# Description
+
+A content management system (CMS) to manage a company's employee database, using Node.js, Inquirer, and MySQL.
+
+# Required packages
+
+[Inquirer package](https://www.npmjs.com/package/inquirer/v/8.2.4)
+
+[MySQL2 package](https://www.npmjs.com/package/mysql2)
+
+[console.table package](https://www.npmjs.com/package/console.table)
+
+
+npm install inquirer@8.2.4
+npm install mysql2
+npm install console.table
---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO department (department_name) VALUES 
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
+
+INSERT INTO role (title, salary, department_id) VALUES 
+('Salesperson', 50000.00, 1),
+('Sales Lead', 75000.00, 1),
+('Lead Engineer', 95000.00, 2),
+('Software Engineer', 75000.00, 2),
+('Accountant', 65000.00, 3),
+('Legal Team Lead', 85000.00, 4),
+('Lawyer', 75000.00, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
+('Tony', 'Stark', 1, NULL),
+('Bruce', 'Banner', 2, 1),
+('Steve', 'Rogers', 3, NULL),
+('Peter', 'Parker', 4, 3),
+('Thor', 'Odinson', 5, 3),
+('Clinton', 'Barton', 6, NULL),
+('Wanda', 'Maximoff', 7, 6);
